00aecc81c6e2a827b70673d3ef8bb7ac0987f588
Azad Dogu <86126162+doguazad@users.noreply.github.com>
2022-01-18 21:07:14 +0300

    Add files via upload

---
+++ 301.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(0);
+ $api = $_GET['engine'];
+ $b = $_GET['browser'];
+
+if($b == 1){
+    echo "You are being redirected...";
+    header("Location: https://www.bing.com/search?q=$api");
+}
+elseif($b == 2){
+    echo "You are being redirected...";
+    header("Location: https://www.google.com/search?q=$api");
+}
+elseif($b == 3){
+    echo "You are being redirected...";
+    header("Location: https://duckduckgo.com/?q=$api");   
+}
+elseif($b == 4){
+    echo "You are being redirected...";
+    header("Location: https://yandex.com.tr/search/?text=$api");
+}
+elseif($b == 5){
+    echo "You are being redirected...";
+    header("Location: https://search.yahoo.com/search?p=$api");
+}
+else{
+    echo "We encountered an Error.We send you back.";
+    header("Refresh: 5; url=index.php");
+}
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body> <center>
+    <form action="301.php" method="GET" name="basket">
+        <h2 id="title">What are you looking for ?</h2>
+        <input type="text" name="engine" id="one" required> <br>
+        <Label id="ex_1"><h2>Select Engines You Want to Search</h2></Label>
+        <Label id="one_b">Bing</Label> <input type="radio" name="browser" value="1" class="engines" > <br>
+        <Label id="two_b">Google</Label> <input type="radio" name="browser" value="2"class="engines"  > <br>
+        <Label id="three_b">DuckDuckGo</Label> <input type="radio" name="browser" value="3" class="engines" > <br>
+        <Label id="four_b">Yandex</Label> <input type="radio" name="browser" value="4" class="engines" > <br>
+        <Label id="five_b">Yahoo</Label> <input type="radio" name="browser" value="5" class="engines" > <br>
+        <button name="basket">Start Search</button>
+    </form> </center>
+</body>
+</html>
